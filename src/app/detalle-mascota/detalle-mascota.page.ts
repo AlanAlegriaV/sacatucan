0d86137d8d5048a58d7ca80b3d3def7484cfee38
AlanAlegriaV <153134463+AlanAlegriaV@users.noreply.github.com>
2024-09-13 18:47:03 -0300

    Update detalle-mascota.page.ts
    se cambian cosas

--- src/app/detalle-mascota/detalle-mascota.page.ts
+++ src/app/detalle-mascota/detalle-mascota.page.ts
@@ -10,12 +10,18 @@ import { NavController } from '@ionic/angular';
 export class DetalleMascotaPage implements OnInit {
 
   mascota: any; // Información de la mascota
-  usuarioActual: any;  // Usuario logueado
+  usuarioActual: any = {};  // Usuario logueado
 
   constructor(private route: ActivatedRoute, private navCtrl: NavController) {}
 
   ngOnInit() {
-    this.usuarioActual = JSON.parse(localStorage.getItem('usuarioActual') || '{}');  // Cargar el usuario logueado
+    const correoLogueado = localStorage.getItem('correoLogueado');  // Obtener el correo del usuario logueado
+
+    if (correoLogueado) {
+      const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');  // Obtener la lista de usuarios
+      this.usuarioActual = usuarios.find((user: any) => user.correo === correoLogueado);  // Buscar el usuario logueado
+    }
+
     this.route.queryParams.subscribe(params => {
       if (params && params['mascota']) {
         this.mascota = JSON.parse(params['mascota']);  // Cargar la mascota seleccionada
@@ -29,19 +35,18 @@ export class DetalleMascotaPage implements OnInit {
   }
 
   // Método para enviar un mensaje al dueño de la mascota
-  // Método para enviar un mensaje al dueño de la mascota
-contactar() {
-  const mensajes = JSON.parse(localStorage.getItem(`mensajes_${this.mascota.correo}`) || '[]');
-  const nuevoMensaje = {
-    texto: `Hola, me encantaría pasear a tu mascota ${this.mascota.nombre}.`, // Incluir el nombre de la mascota
-    nombre: this.usuarioActual.nombre,
-    apellido: this.usuarioActual.apellido,
-    correo: this.usuarioActual.correo,
-    telefono: this.usuarioActual.telefono
-  };
-  mensajes.push(nuevoMensaje);
-  localStorage.setItem(`mensajes_${this.mascota.correo}`, JSON.stringify(mensajes));
-  alert('Mensaje enviado exitosamente');
-}
+  contactar() {
+    const mensajes = JSON.parse(localStorage.getItem(`mensajes_${this.mascota.correo}`) || '[]');
+    const nuevoMensaje = {
+      texto: `Hola, me encantaría pasear a tu mascota ${this.mascota.nombre}.`,  // Incluir el nombre de la mascota
+      nombre: this.usuarioActual.nombre,
+      apellido: this.usuarioActual.apellido,
+      correo: this.usuarioActual.correo,
+      telefono: this.usuarioActual.telefono
+    };
+    mensajes.push(nuevoMensaje);
+    localStorage.setItem(`mensajes_${this.mascota.correo}`, JSON.stringify(mensajes));
+    alert('Mensaje enviado exitosamente');
+  }
 
 }
