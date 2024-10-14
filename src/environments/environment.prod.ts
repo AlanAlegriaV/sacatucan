7af8148900475168ddd5b33592f1806fa5c689e2
jsgiaverini <juliosoto2011@gmail.com>
2024-10-14 16:25:00 -0300

    add config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDe7x45_Fy4fel2QZQkTdWaVAZgNaULrxM",
+    authDomain: "sacatucan.firebaseapp.com",
+    projectId: "sacatucan",
+    storageBucket: "sacatucan.appspot.com",
+    messagingSenderId: "162901916057",
+    appId: "1:162901916057:web:8bca35835484cecb237b08"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDe7x45_Fy4fel2QZQkTdWaVAZgNaULrxM",
+    authDomain: "sacatucan.firebaseapp.com",
+    projectId: "sacatucan",
+    storageBucket: "sacatucan.appspot.com",
+    messagingSenderId: "162901916057",
+    appId: "1:162901916057:web:8bca35835484cecb237b08"
+  },
 };
 
 /*
