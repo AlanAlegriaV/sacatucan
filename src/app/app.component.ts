bb24f8dfc511cccdcb4e41cb11cc1c699c75826c
jsgiaverini <juliosoto2011@gmail.com>
2024-10-22 15:23:05 -0300

    ajuste en cerrar sesion y verificar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router'; // Importamos Router para hacer la redirección
 import { MenuController } from '@ionic/angular'; // Importamos el controlador de menú
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { MenuController } from '@ionic/angular'; // Importamos el controlador de
 export class AppComponent {
   usuarioAutenticado: boolean = false;  // Variable para saber si hay un usuario autenticado
 
-  constructor(private router: Router, private menuCtrl:  MenuController) {
+  constructor(private router: Router, private menuCtrl:  MenuController, private afAuth: AngularFireAuth) {
     // Escuchar los cambios de ruta para verificar la autenticación cada vez que el usuario navega
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
@@ -41,34 +42,21 @@ export class AppComponent {
     });
   }
 
-
-
-  ngOnInit() {
-    this.verificarAutenticacion();  // Verificar la autenticación al iniciar la aplicación
+  verificarAutenticacion(): boolean{
+    return this.afAuth.authState !== null;
   }
 
-  // Método para verificar si hay un usuario autenticado
-  verificarAutenticacion() {
-    const correoLogueado = localStorage.getItem('correoLogueado');
-    if (correoLogueado) {
-      this.usuarioAutenticado = true;  // Si existe un correo en el localStorage, hay un usuario autenticado
-    } else {
-      this.usuarioAutenticado = false;  // Si no hay un correo, no hay usuario autenticado
-    }
-  }
-
+  
   // Función para cerrar sesión
-  cerrarSesion() {
-    this.menuCtrl.close().then(() => {
-    // Eliminar la información de sesión (correo logueado)
-    localStorage.removeItem('correoLogueado');  
-    localStorage.removeItem('usuarioActual'); // También puedes eliminar otros datos si es necesario
-
-    // Cambiar el estado de autenticación
-    this.usuarioAutenticado = false;
-
-    // Redirigir al usuario a la página de login
-    this.router.navigate(['/login']);
-    });
+  async cerrarSesion(): Promise<any> {
+    try {
+      await this.menuCtrl.close(); //cerrar el menu lateral
+      await this.afAuth.signOut();
+      console.log('Sesion cerrada satisfactoriamente');
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.log('Error al cerrar sesion', error);
+      throw error;
+    }
   }
 }
